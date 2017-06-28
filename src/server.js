ae04ed3896f311dec81ff416add0872e41bf1848
ironpup <christoph.ding@gmail.com>
2017-06-28 11:18:23 -0400

    Now server is serving a get request to the root, albeit very simple.

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+})
+
+app.listen(port);
+console.log('listening on port ', port)
