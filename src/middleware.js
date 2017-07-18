65c2de43658394b2d41884717c616d4158142417
ironpup <christoph.ding@gmail.com>
2017-07-17 21:06:09 -0400

    Clean up middleware a little.

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,10 @@
-
 module.exports = function(app) {
   console.log('inside middleware...')
   
   require('./services/logging.js')(app)
 
   app.use(function(req, res) {
-    console.log('here')
+    console.log('final function before sending response')
     res.send()
   })  
 
