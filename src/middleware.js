d4f3631d6d2036e2758ff5e93ab6d32f0bb9e13b
ironpup <christoph.ding@gmail.com>
2017-07-21 16:58:14 -0400

    Add some extra callback on login to test my memory of callback chains.  Start some endpoints that will later be made into routes.

--- src/middleware.js
+++ src/middleware.js
@@ -3,6 +3,16 @@ module.exports = function(app) {
   
   require('./services/logging.js')(app)
 
+  // the endpoints are here for now,
+  // will make into routers
+
+  app.get('/friends', function(req, res, next) {
+    console.log('GETTING friends')
+    next()
+  })
+
+
+
   app.use(function(req, res) {
     console.log('final function before sending response')
     res.send()
--- src/services/logging.js
+++ src/services/logging.js
@@ -1,11 +1,16 @@
-var testLog = function(req, res, next) {
-  console.log('test log')
+var testLogOne = function(req, res, next) {
+  console.log('test log: function 1')
+  next()
+}
+
+var testLogTwo = function(req, res, next) {
+  console.log('test log: function 2')
   next()
 }
 
 module.exports = function(app) {
   console.log('inside logging...')
 
-  app.get('/test', testLog)
+  app.get('/test', testLogOne, testLogTwo)
 }
 
