dd1bd3ab6b1d86cdfe2558e2e9a911b0a67e928d
ironpup <christoph.ding@gmail.com>
2017-07-21 17:14:03 -0400

    Continue to add stubs for endpoints.

--- src/middleware.js
+++ src/middleware.js
@@ -11,7 +11,20 @@ module.exports = function(app) {
     next()
   })
 
+  app.get('/friends/:id', function(req, rex, next) {
+    console.log('GETTING friend: ', req.params.id)
+    next()
+  })
 
+  app.get('/user', function(req, res, next) {
+    console.log('GETTING user')
+    next()
+  })
+
+  app.get('/user/:id', function(req, res, next) {
+    console.log('GETTING user: ', req.params.id)
+    next()
+  })
 
   app.use(function(req, res) {
     console.log('final function before sending response')
