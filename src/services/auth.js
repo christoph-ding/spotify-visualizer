f41bd8a3915cd0e30f1ca6d7ac47eb535df739cf
ironpup <christoph.ding@gmail.com>
2017-08-03 22:21:07 -0400

    Add request module to app.

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,12 @@
 var credentials = require('./user_creds.js')
+var request = require('request')
 
 function testAuth(req, res, next) {
   console.log('authing ... ')
   console.log(credentials)
+
+  // make a get request with our credentials
+
   next()
 }
 
