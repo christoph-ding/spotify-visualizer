3d7dd18a3c607e543124bf09486694b948bebed3
ironpup <christoph.ding@gmail.com>
2017-08-13 11:38:16 -0400

    Small aesthetic change in user and friends controller - changed 'exports' into 'module.exports'

--- src/services/friends/friends-controller.js
+++ src/services/friends/friends-controller.js
@@ -1,14 +1,14 @@
-exports.listFriends = function(req, res, next) {
+module.exports.listFriends = function(req, res, next) {
   console.log('GETTING friends')
   next()
 }
 
-exports.listSpecificFriend = function(req, res, next) {
+module.exports.listSpecificFriend = function(req, res, next) {
   console.log('GETTING friend: ', req.params.id)
   next()
 }
 
-exports.sendMessage = function(req, res, next) {
+module.exports.sendMessage = function(req, res, next) {
   console.log('POSTING to friend: ', req.params.id)
   console.log('with msg: ', req.body)
   next()  
--- src/services/user/user-controller.js
+++ src/services/user/user-controller.js
@@ -1,9 +1,9 @@
-exports.listUser = function(req, res, next) {
+module.exports.listUser = function(req, res, next) {
   console.log('GETTING user')
   next()
 }
 
-exports.listSpecificUser = function(req, res, next) {
+module.exports.listSpecificUser = function(req, res, next) {
   console.log('GETTING user: ', req.params.id)
   next()
 }
