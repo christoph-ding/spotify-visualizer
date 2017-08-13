70887ffd9cf1eadd1cc51512cdf274f0f816a6d4
ironpup <christoph.ding@gmail.com>
2017-08-12 22:05:50 -0400

    Make progress on the auth system.

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,20 +1,33 @@
 var credentials = require('./user_creds.js')
 var request = require('request')
 
+
 function testAuth(req, res, next) {
   console.log('authing ... ')
   console.log(credentials)
 
+  var clientID = credentials.Client_ID
+  var clientSecret = credentials.Client_Secret
+
   // make a get request with our credentials
-  var url = 'http://www.google.com'
+  var spotifyAuthRoot = 'https://accounts.spotify.com/authorize'
+  var redirect_uri = 'https://www.google.com'
+  var authenticationURL = spotifyAuthRoot + '?client_id=' + clientID + '&response_type=code' + '&redirect_uri=' + redirect_uri
 
   function test(err, res, body) {
     console.log(res.statusCode)
     next()
   }
 
-  request(url, test)
+  console.log(authenticationURL)
+
+  request(authenticationURL, test)
 }
 
 
 module.exports.testAuth = testAuth
+
+// return 'https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID +
+//               '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+//               '&scope=' + encodeURIComponent(scopes.join(' ')) +
+//               '&response_type=token';
