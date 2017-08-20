03d8bd76cb733794d4a4e55778d9b4af4fa4b878
ironpup <christoph.ding@gmail.com>
2017-08-20 15:11:41 -0400

    Start refactoring the auth controller and router to handle redirecting to a login-landing page.

--- src/services/authentication/auth-controller.js
+++ src/services/authentication/auth-controller.js
@@ -3,31 +3,29 @@ var request = require('request')
 var path = require('path')
 var express = require('express')
 
-module.exports.testAuth = function(req, res, next) {
-  console.log('sending the login page')
-  next()
+module.exports.login = function(req, res, next) {
+  console.log('logging in ... ')
+
+  var clientID = credentials.Client_ID
+  var clientSecret = credentials.Client_Secret
+
+  var spotifyAuthRoot = 'https://accounts.spotify.com/authorize'
+  var redirect_uri = 'https://www.google.com'
+  var authenticationURL = spotifyAuthRoot + '?client_id=' + clientID + '&response_type=code' + '&redirect_uri=' + redirect_uri + '&response_type=token'
+
+  console.log(authenticationURL)
+
+  request(authenticationURL, next)
 }
 
-module.exports.login = function(req, res, next) {
+module.exports.loginLanding = function(req, res, next) {
   var filePath = path.resolve(__dirname + '/../../../public/login-landing.html')
   console.log(filePath)
   res.sendFile(filePath)
   // next()
 }
 
-// function testAuth(req, res, next) {
-//   console.log('authing ... ')
-//   console.log(credentials)
-
-//   var clientID = credentials.Client_ID
-//   var clientSecret = credentials.Client_Secret
-
-//   // make a get request with our credentials
-//   var spotifyAuthRoot = 'https://accounts.spotify.com/authorize'
-//   var redirect_uri = 'https://www.google.com'
-//   var authenticationURL = spotifyAuthRoot + '?client_id=' + clientID + '&response_type=code' + '&redirect_uri=' + redirect_uri
-
-//   console.log(authenticationURL)
+  // make a get request with our credentials
 
 //   request(authenticationURL, next)
 // }
